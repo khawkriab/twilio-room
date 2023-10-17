4413cd69239d8adb6ea95c506b1d0fef3a335feb
khawkriab <khawkriab.dev@outlook.com>
2023-10-17 21:22:13 +0700

    update: svg icon

--- src/components/MenuBar/UserAvatar/UserAvatar.tsx
+++ src/components/MenuBar/UserAvatar/UserAvatar.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import makeStyles from '@material-ui/styles/makeStyles';
-import Person from '@material-ui/icons/Person';
-import { StateContextType } from '../../../state';
+import React from 'react'
+import Avatar from '@material-ui/core/Avatar'
+import Person from '@material-ui/icons/Person'
+import { StateContextType } from '../../../state'
+import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles({
-  red: {
-    color: 'white',
-    backgroundColor: '#F22F46',
-  },
-});
+    red: {
+        color: 'white',
+        backgroundColor: '#F22F46'
+    }
+})
 
 export function getInitials(name: string) {
-  return name
-    .split(' ')
-    .map(text => text[0])
-    .join('')
-    .toUpperCase();
+    return name
+        .split(' ')
+        .map((text) => text[0])
+        .join('')
+        .toUpperCase()
 }
 
 export default function UserAvatar({ user }: { user: StateContextType['user'] }) {
-  const classes = useStyles();
-  const { displayName, photoURL } = user!;
+    const classes = useStyles()
+    const { displayName, photoURL } = user!
 
-  return photoURL ? (
-    <Avatar src={photoURL} />
-  ) : (
-    <Avatar className={classes.red}>{displayName ? getInitials(displayName) : <Person />}</Avatar>
-  );
+    return photoURL ? (
+        <Avatar src={photoURL} />
+    ) : (
+        <Avatar className={classes.red}>{displayName ? getInitials(displayName) : <Person />}</Avatar>
+    )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -33,6 +33,6 @@ export const TwilioRoom = ({ token }: AppProps) => {
     )
 }
 
-// const t =
-//     'eyJjdHkiOiJ0d2lsaW8tZnBhO3Y9MSIsInR5cCI6IkpXVCIsImFsZyI6IkhTMjU2In0.eyJpc3MiOiJTSzhmMTY0ZTRiYTQ1MDIwYWM5NWNhYjg4MzA5ZDFjZjNhIiwiZXhwIjoxNjk3NTE4NDYwLCJncmFudHMiOnsiaWRlbnRpdHkiOiJzcyIsInZpZGVvIjp7InJvb20iOiJQSEFSTTAzMjMxMDE3LTAwMDAxIn19LCJqdGkiOiJTSzhmMTY0ZTRiYTQ1MDIwYWM5NWNhYjg4MzA5ZDFjZjNhLTE2OTc1MTQ4ODAiLCJzdWIiOiJBQ2U3MWY5ZjgyMmZiNzQxNTE3ZGJiNGFjMzBjMzU0YWYwIn0.-qWnnprZ7Gd0heKNgjhpZZztxHGh7TauTz0DRb8gPcU'
-// ReactDOM.render(<TwilioRoom token={t} />, document.getElementById('root'))
+const t =
+    'eyJjdHkiOiJ0d2lsaW8tZnBhO3Y9MSIsInR5cCI6IkpXVCIsImFsZyI6IkhTMjU2In0.eyJpc3MiOiJTSzhmMTY0ZTRiYTQ1MDIwYWM5NWNhYjg4MzA5ZDFjZjNhIiwiZXhwIjoxNjk3NTE4NDYwLCJncmFudHMiOnsiaWRlbnRpdHkiOiJzcyIsInZpZGVvIjp7InJvb20iOiJQSEFSTTAzMjMxMDE3LTAwMDAxIn19LCJqdGkiOiJTSzhmMTY0ZTRiYTQ1MDIwYWM5NWNhYjg4MzA5ZDFjZjNhLTE2OTc1MTQ4ODAiLCJzdWIiOiJBQ2U3MWY5ZjgyMmZiNzQxNTE3ZGJiNGFjMzBjMzU0YWYwIn0.-qWnnprZ7Gd0heKNgjhpZZztxHGh7TauTz0DRb8gPcU'
+ReactDOM.render(<TwilioRoom token={t} />, document.getElementById('root'))
